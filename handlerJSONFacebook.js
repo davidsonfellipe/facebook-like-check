894e9630c3eee4dfef006a69afb4567af7db07e5
davidson <email@fellipe.com>
2011-10-23 22:01:02 -0200

    minify files

--- handlerJSONFacebook.js
+++ handlerJSONFacebook.js
@@ -1,6 +1,6 @@
 function handlerJSONFacebook(response){  
-
-	var HTML = "<table id='tablesorter' class='tablesorter'>" +
+    "use strict";
+    var HTML = "<table id='tablesorter' class='tablesorter'>" +
                 "<thead>" +
                 "<tr>" + 
                     "<th> url </th>" +
@@ -11,8 +11,8 @@ function handlerJSONFacebook(response){
                 "</thead>" + 
                 "<tbody>";
 
-	$.each(response, function(index, object) { 
-        
+    $.each(response, function(index, object) { 
+    
         HTML += "<tr class='table-content'>" + 
                     "<td><a href='" + object.normalized_url + "' target='_blank'>" + object.url + "</a></td>" +
                     "<td>" + object.like_count + "</td>" +
@@ -30,9 +30,9 @@ function handlerJSONFacebook(response){
 }
 
 function handlerXMLtoJSONSitemap(response){  
-
+    "use strict";
     var urls = "";
-    
+
     if(response.query.results.sitemapindex){
         $("#result").html('<div class="error">sitemapindex is not supported</div>');
     }
@@ -40,20 +40,20 @@ function handlerXMLtoJSONSitemap(response){
         $.each(response.query.results.urlset.url, function(index, object) { 
             urls += object.loc + ',';
         });
-        
+    
         urls = encodeURIComponent(urls);
 
         var url = "http://api.facebook.com/restserver.php?method=links.getStats&urls=%20" + urls + "&format=json&callback=handlerJSONFacebook";
 
         $.getScript(url);
     }else{
-        //response.query.results.urlset.url
+        /*response.query.results.urlset.url*/
         $("#result").html('<div class="error">sitemap too large or badly formatted</div>');
     }
 }
 
 $(document).ready(function(){
-    
+    "use strict"; 
     $('#get-info').click(function() {
         
         var urls = encodeURIComponent($('#urls').val().replace(/(\r\n|\n|\r)/gm, ","));
@@ -74,6 +74,7 @@ $(document).ready(function(){
     
 
     $('.tab', ".tabs").click(function(){
+        
         $(".content",".tabs").hide();
         $(".content",".tabs").removeClass("content-active");
         $($(this).attr("rel")).show();
